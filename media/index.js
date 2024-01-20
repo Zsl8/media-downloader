28501e0eaccbcf5c071bb648d06ad1632c1f474b
Zsl8 <71377428+Zsl8@users.noreply.github.com>
2024-01-20 17:40:20 +0300

    Create index.js

---
+++ media/index.js
@@ -0,0 +1,9 @@
+const Instagram = require('./instagram')
+const Tiktok = require('./tiktok')
+const Youtube = require('./youtube')
+
+module.exports = {
+    Instagram,
+    Tiktok,
+    Youtube,
+}
